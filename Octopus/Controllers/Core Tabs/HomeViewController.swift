ca4b7a1cd622beeb565bcd35d6aa8bfc47dc123c
Sergey Samokhvalov <svsamokhvalov@domclick.ru>
2020-11-07 23:01:01 +0300

    init controllers, Podfile

---
+++ Octopus/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Octopus
+//
+//  Created by Самохвалов Сергей Владимирович on 06.11.2020.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
